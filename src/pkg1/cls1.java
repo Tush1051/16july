7a9ef66181271fa9478d1bcd94007c0ce2078d34
Lenovo <Lenovo@DESKTOP-1OFM8BQ>
2022-12-01 14:37:45 +0530

    [tush]new project

---
+++ src/pkg1/cls1.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class cls1 {
+	public static void main(String[] args) {
+		System.out.println("write");
+		System.out.println("good morning eyerone");
+		System.out.println("asdffmmfj");
+	}
+
+}
